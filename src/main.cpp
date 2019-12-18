7d336fac566d095690a248127984a1258b36be83
andrewcgaitskell <andrew@gaitskell.com>
2019-12-18 12:31:50 +0000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -75,6 +75,7 @@ void setup() {
 
 void loop() {
   ArduinoOTA.handle();
+  Serial.print("IP address: ");
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
